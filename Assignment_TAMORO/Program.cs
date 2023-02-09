b066ac1e9dc3e0028a9d033fc1087f52fd429aca
Administrator <Administrator@DESKTOP-FUGQNF4>
2023-02-09 17:43:59 +0800

    Add project files.

---
+++ Assignment_TAMORO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 300; i++)
+            {
+                GetOddNumberDivisiblebyFive(i);
+            }
+        }
+
+        private static void GetOddNumberDivisiblebyFive(int i)
+        {
+            if ((i % 2) != 0 && (i % 5) == 0)
+            {
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
